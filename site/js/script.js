4374ab90b2fb61ef04e5fd8a922ede6902fc2822
Ismael <lindoespinoso@gmail.com>
2023-03-14 22:34:47 +0100

    website

--- site/js/script.js
+++ site/js/script.js
@@ -42,35 +42,6 @@
 			maps:                    $( ".google-map-container" ),
 			particlesJs:             $( '#particles-js' )
 		};
-		const {google} = require('googleapis');
-
-		/**
-		 * To use OAuth2 authentication, we need access to a CLIENT_ID, CLIENT_SECRET, AND REDIRECT_URI
-		 * from the client_secret.json file. To get these credentials for your application, visit
-		 * https://console.cloud.google.com/apis/credentials.
-		 */
-		const oauth2Client = new google.auth.OAuth2(
-			"420503227116-4an8t81mgrb2q0897de7fu3ggs5jbima.apps.googleusercontent.com",
-		  TOKENSECRET,
-		  "https://fancydeath-c7991.firebaseapp.com/__/auth/handler"
-		);
-		
-		// Access scopes for read-only Drive activity.
-		const scopes = [
-		  'https://www.googleapis.com/auth/drive.metadata.readonly'
-		];
-		
-		// Generate a url that asks permissions for the Drive activity scope
-		const authorizationUrl = oauth2Client.generateAuthUrl({
-		  // 'online' (default) or 'offline' (gets refresh_token)
-		  access_type: 'offline',
-		  /** Pass in the scopes array defined above.
-			* Alternatively, if only one scope is needed, you can pass a scope URL as a string */
-		  scope: scopes,
-		  // Enable incremental authorization. Recommended as a best practice.
-		  include_granted_scopes: true
-		});
-		res.writeHead(301, { "Location": authorizationUrl });
 
 	/**
 	 * @desc Check the element was been scrolled into the view
@@ -81,20 +52,6 @@
 		if ( isNoviBuilder ) return true;
 		return elem.offset().top + elem.outerHeight() >= $window.scrollTop() && elem.offset().top <= $window.scrollTop() + $window.height();
 	}
-	// Your web app's Firebase configuration
-		// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-		const firebaseConfig = {
-			apiKey: "AIzaSyAgNeQXYmUg-EEgkZ0x_EbY8x3jfIDmoPo",
-			authDomain: "fancydeath-c7991.firebaseapp.com",
-			projectId: "fancydeath-c7991",
-			storageBucket: "fancydeath-c7991.appspot.com",
-			messagingSenderId: "420503227116",
-			appId: "1:420503227116:web:7f6d0fb980a32b479a73c9",
-			measurementId: "G-4ZSP05CBP1"
-		  };
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
 
 	/**
 	 * @desc Calls a function when element has been scrolled into the view
